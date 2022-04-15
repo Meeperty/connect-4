8843dba942142fe3665723606a834e040c8f9475
Meeperty <meepprograms@gmail.com>
2022-04-14 17:11:57 -0700

    game end UI and resetting board

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -15,6 +15,9 @@ namespace Connect4
         public Sprite yellow;
         public Sprite red;
 
+        public GameObject winTextGameObject;
+        public GameObject winUI;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -57,8 +60,7 @@ namespace Connect4
                     }
                     if (PositionEval.HasPlayerWon(new Vector2Int(column, r), currentPlayer))
                     {
-                        Debug.Log("player " + currentPlayer + " has won");
-                        ResetBoard();
+                        GameWin(currentPlayer);
                     }
                     else
                     {
@@ -69,10 +71,25 @@ namespace Connect4
             }
         }
 
+        public void GameWin(int winner)
+        {
+            Debug.Log("player " + winner + " has won");
+            if (winner == 1)
+            {
+                winTextGameObject.GetComponent<Text>().text = "Yellow player has won";
+            }
+            else
+            {
+                winTextGameObject.GetComponent<Text>().text = "Red player has won";
+            }
+            winUI.SetActive(true);
+        }
+
         public void NextTurn()
         {
             if (currentPlayer == 1) { currentPlayer++; }
             else { currentPlayer = 1; }
+            MatchGraphicsToStates();
         }
 
         public void ShowHideSquares(bool activated)
@@ -97,6 +114,32 @@ namespace Connect4
                     currentPlayer = 1;
                 }
             }
+            winUI.SetActive(false);
+        }
+
+        public void MatchGraphicsToStates()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    squares[i, j].GetComponent<SpriteRenderer>().sprite = SquareStateToSprite(squareStates[i, j]);
+                }
+            }
+        }
+
+        public Sprite SquareStateToSprite(SquareState state)
+        {
+            switch (state)
+            {
+                case SquareState.Empty:
+                    return empty;
+                case SquareState.Red:
+                    return red;
+                case SquareState.Yellow:
+                    return yellow;
+            }
+            throw new ImpossibleException($"Recived squareState {state}");
         }
     }
 
@@ -105,5 +148,17 @@ namespace Connect4
         Empty,
         Red,
         Yellow
+    }
+
+
+    [System.Serializable]
+    public class ImpossibleException : System.Exception
+    {
+        public ImpossibleException() { }
+        public ImpossibleException(string message) : base(message) { }
+        public ImpossibleException(string message, System.Exception inner) : base(message, inner) { }
+        protected ImpossibleException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
